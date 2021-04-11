c0844b2bfe7bf590df7d9939b3be2100ecd8b3d0
Michael-Khanna <mikekhanna@icloud.com>
2021-04-10 19:12:07 -0700

    Revised HTML structure and changed JS module

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 "use strict";
 
-(function () {
+let dropDownMenu = (function () {
   let btns = document.querySelectorAll(`.menuOpener`);
   let divs = document.querySelectorAll(`.vertical-menu`);
   for (let i = 0; i < btns.length; i++) {
     let links = divs[i].querySelectorAll(`a`);
-    for(let i = 0; i < links.length; i++){
-        links[i].addEventListener(`click`, function(){
-            for(let j = 0; j < links.length; j++){
-                links[j].classList.remove(`active`);
-            }
-            links[i].classList.add(`active`);
-        });
+    for (let i = 0; i < links.length; i++) {
+      links[i].addEventListener(`click`, function () {
+        for (let j = 0; j < links.length; j++) {
+          links[j].classList.remove(`active`);
+        }
+        links[i].classList.add(`active`);
+      });
     }
     btns[i].addEventListener(`click`, function () {
       if (divs[i].classList.contains(`concealed`)) {
@@ -22,3 +22,5 @@
     });
   }
 })();
+
+export { dropDownMenu };
